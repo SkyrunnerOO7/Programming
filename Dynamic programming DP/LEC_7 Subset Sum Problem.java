3270a72e4ff781ab016653320d1d5cfa408be5f5
Punit pawar <70194383+SkyrunnerOO7@users.noreply.github.com>
2022-07-27 10:57:56 +0530

    LEC_7

---
+++ Dynamic programming DP/LEC_7 Subset Sum Problem.java
@@ -0,0 +1,31 @@
+//Question link : https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
+class Solution{
+    static Boolean isSubsetSum(int n, int arr[], int sum){
+        // code here
+        
+        boolean t[][] = new boolean[n+1][sum+1];
+        
+        //initilization true and false
+        for(int i = 0; i<sum+1; i++)
+        {t[0][i] = false;}
+        
+        for(int i = 0; i<n+1; i++)
+        {t[i][0] = true;}
+
+        
+        for(int i =1; i<n+1; i++)
+        {
+            for(int j = 1; j<sum+1; j++)
+            {
+                if(arr[i-1] <= j)
+                {
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][sum];
+    }
+}
